70ec608a7ed161c5cc096aa2158a6c6f4e5e8239
VedantS73 <vedantsawant72003@gmail.com>
2023-08-27 12:02:16 +0530

    string logic

--- src/App.jsx
+++ src/App.jsx
@@ -35,35 +35,50 @@ import Spreadsheet from "react-spreadsheet";
 
 const App = () => {
   const [data, setData] = useState([
-    [{ value: "aaa" }, { value: "ccc" }],
-    [{ value: "bbb" }, { value: "ddd" }],
+    [{ value: "" }, { value: "" }],
+    [{ value: "" }, { value: "" }],
+    [{ value: "" }, { value: "" }],
+    [{ value: "" }, { value: "" }],
+    [{ value: "" }, { value: "" }],
+    [{ value: "" }, { value: "" }],
   ]);
   let col = ["Req no","Wip Line id","Product","Collection","TOC Order","Department"]
 
-  return <Spreadsheet data={data} 
-  onChange={(d)=>{
-    const t = d
-    const string = d[0][2]
-    console.log("str is ",string)
-    
-    if(string === '1'){
-      t[0][5] = "SS"
-    }
-    else if(string === "2"){
-      t[0][5] = "CASTING"
-    }
-    else if(string === "3"){
-      t[0][5] = "JUNA"
-    }
-    else if(string === "4"){
-      t[0][5] = "GHAT"
-    }
-    else {
-      t[0][5] = "none"
-    }
-    setData(t)
-  }} 
-  columnLabels={col}/>;
+  return (
+    <>
+      <Spreadsheet data={data} 
+      onChange={(d)=>{
+        const t = d
+        for (let i = 0; i < 6; i++) {
+          const string = d[i][2]
+          console.log("str is ",string) 
+          if(string.value[0] === '1'){
+            t[i][5] = {value:"SS",readOnly:true}
+          }
+          else if(string.value[0] === "2"){
+            t[i][5] = {value:"CASTING",readOnly:true}
+          }
+          else if(string.value[0] === "3"){
+            t[i][5] = {value:"JUNA",readOnly:true}
+          }
+          else if(string.value[0] === "4"){
+            t[i][5] = {value:"GHAT",readOnly:true}
+          }
+          else {
+            t[i][5] = {value:"",readOnly:true}
+          }
+          setData(t)
+        }
+      }} columnLabels={col} />
+      <p>Just an implementation of requirement</p>
+      <p>Implementing String logic in sheets (this is just and exapmle):
+        <li>1st char of Product is 1 then Department is SS</li>
+        <li>1st char of Product is 2 then Department is CASTING</li>
+        <li>1st char of Product is 3 then Department is JUNA</li>
+        <li>1st char of Product is 4 then Department is GHAT</li>
+      </p>
+    </>
+  )  
 };
 
 export default App
